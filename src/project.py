432e9110304b5a03e61bf3d3cf4e5a761d5c2bd0
Eli Arrell <eliarrell@icloud.com>
2025-12-03 20:04:07 -0600

    created project outline and initial setup

--- src/project.py
+++ src/project.py
@@ -1,4 +1,14 @@
 from minecraftstuff import Minecraft
-mc = Minecraft.create()
-mc.postToChat("Hello World!")
+from PIL import Image
+import math
 
+# connecting to the Minecraft server
+
+
+# set where the image will start in the Minecraft world
+
+
+# function to color match the image pixel to the closest Minecraft block color
+
+
+# resize image to fit within Minecraft world constraints and build it block by block
